de7e8c70615106e2b0b547e2ff9af1d2b3119ddf
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-02-05 14:00:13 -0300

    new file:   garbagecollection.cpp

---
+++ garbagecollection.cpp
@@ -0,0 +1,26 @@
+#include <math.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class disposing{};
+
+class garbagecollection
+{
+private:
+    /* data */
+public:
+    garbagecollection(/* args */);
+    ~garbagecollection();
+};
+
+garbagecollection::garbagecollection(/* args */)
+{
+}
+
+garbagecollection::~garbagecollection()
+{
+}
