0e99a26ff8c84faed142b30e060be0306ae57b8a
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-02-05 14:16:49 -0300

    update garbaagecollection

--- garbagecollection.cpp
+++ garbagecollection.cpp
@@ -2,25 +2,52 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 
 using namespace std;
 
 class disposing{};
 
-class garbagecollection
+class GarbageCollection
 {
 private:
-    /* data */
+   
+    std::unordered_map<void*,int>refCount;
+
 public:
-    garbagecollection(/* args */);
-    ~garbagecollection();
+   
+   void add(void*obj){
+    refCount[obj]=1;
+   }
+
+   void incrementRef(void* obj) {
+    if (obj && refCount.find(obj) != refCount.end()) {
+            refCount[obj]++;
+        }
+   }
+
+   void decrementRef(void* obj) {
+
+    if (obj && refCount.find(obj) != refCount.end()) {
+
+            refCount[obj]--;
+
+            if (refCount[obj] == 0) {
+                delete (int*)obj; 
+                refCount.erase(obj);
+                std::cout << "Memoria livre!\n";
+            }
+        }
+   }
+
 };
 
-garbagecollection::garbagecollection(/* args */)
-{
-}
 
-garbagecollection::~garbagecollection()
-{
-}
+GarbageCollection gc;
+
+class PontoIntelig{
+
+};
+
+
